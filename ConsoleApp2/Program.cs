08a595c66acde6c0b6e5477cdedf5c8dd6de8b0e
ispan <ispan@DESKTOP-PAKSETB>
2022-10-20 17:24:18 +0800

    加入專案檔案。

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+             class Address
+        {
+            // prop
+            public string City { get; set; }
+
+            //public string City { get; set; }
+            public string ZipCode { get; set; }
+            public string Street { get; set; }
+        }
+        class Telephone
+        {
+            public string CountryCode { get; set; }
+            public string AreaCode { get; set; }
+            public string Number { get; set; }
+
+            public string Ext { get; set; }
+        }
+
+        class Member
+        {
+            public string Name { get; set; }
+
+            public string Account { get; set; }
+            public string Email { get; set; }
+            public string password { get; set; }
+            public Address Addr { get; set; }
+            public string Cellphone { get; set; }
+            public Telephone Telephone { get; set; }
+
+            public void Register(string Name, string Account,
+                                string Password, string confirmPassword,
+                                 string Email)
+            {
+
+            }
+
+
+            public void ForgetPassword(string name, string email)
+            {
+
+            }
+
+            public void Aunthenticate(string Account, string Password)
+            {
+
+            }
+
+        }
+    }
+}
